9003cb90196ece826f0290c6f66ab6a76307c3cf
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-01 11:07:58 +0500

    123

---
+++ Weapon.cs
@@ -0,0 +1,31 @@
+class Weapon
+{
+    private int _damage;
+    private int _bullets;
+
+    public void Fire(Player player)
+    {
+        player.TakeDamage(_damage);
+        _bullets--;
+    }
+}
+
+class Player
+{
+    private int _health;
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+    }
+}
+
+class Bot
+{
+    private Weapon _weapon;
+
+    public void OnSeePlayer(Player player)
+    {
+        _weapon.Fire(player);
+    }
+}
